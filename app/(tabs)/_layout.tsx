a2a88a7511d6159c464b5d51856dddf382071e81
xyugen <alex.arias.jorym@gmail.com>
2025-12-23 01:50:56 +0800

    fix: safe area view

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,18 +1,5 @@
-import { Container } from '@/components/Container';
-import React from 'react';
-import { useColorScheme } from 'react-native';
-
 import { AppTabs } from '@/components/app-tabs';
-import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
-import '@/global.css';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-  return (
-    <GluestackUIProvider mode={colorScheme === 'dark' ? 'dark' : 'light'}>
-      <Container>
-        <AppTabs />
-      </Container>
-    </GluestackUIProvider>
-  );
+  return <AppTabs />;
 }
--- components/Container.tsx
+++ components/Container.tsx
@@ -1,9 +1,5 @@
-import { Platform, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export const Container = ({ children }: { children: React.ReactNode }) => {
-  if (Platform.OS === 'android')
-    return <SafeAreaView className="bg-background flex flex-1 px-4">{children}</SafeAreaView>;
-
-  return <View className="flex flex-1">{children}</View>;
+  return <SafeAreaView className="flex flex-1">{children}</SafeAreaView>;
 };
